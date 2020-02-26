9bae6f707c389283f001052760de7ee3ca825235
Ryan Pinnock <ryanpinnock10@gmail.com>
2020-02-25 20:56:32 -0500

    Connected Firebase and React

--- frontend/firebase-react-user/src/components/Admin/index.js
+++ frontend/firebase-react-user/src/components/Admin/index.js
@@ -7,3 +7,19 @@ const Admin = () => (
 );
 
 export default Admin;
+
+/**
+ * This was to test if the Firebase platform and React 
+ * framework was connected
+ */
+
+// import React from 'react';
+// import  { FirebaseContext } from '../Firebase';
+// const SomeComponent = () => (
+//   <FirebaseContext.Consumer>
+//     {firebase => {
+//       return <div>I've access to Firebase and render something.</div>;
+//     }}
+//   </FirebaseContext.Consumer>
+// );
+// export default SomeComponent;
