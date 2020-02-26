cab79b9500bb5b03fae1d11a87c2473f037c0428
Ryan Pinnock <ryanpinnock10@gmail.com>
2020-02-25 21:06:21 -0500

    User functionality enabled via firebase auth/API

--- frontend/firebase-react-user/src/components/Firebase/firebase.js
+++ frontend/firebase-react-user/src/components/Firebase/firebase.js
@@ -1,4 +1,5 @@
 import app from 'firebase/app';
+import 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -14,7 +15,27 @@ const firebaseConfig = {
 class Firebase{
     constructor(){
         app.initializeApp(firebaseConfig);
+
+        this.auth = app.auth();
     }
+
+
+/**
+ * AUTH API
+ */
+
+ doCreateUserWithEmailAndPassword = (email, password) =>
+ this.auth.doCreateUserWithEmailAndPassword(email, password);
+
+ doSignInWithEmailAndPassword= (email, password) =>
+ this.auth.signInWithEmailAndPassword(email, password);
+
+ doSignOut = () => this.auth.signOut();
+
+ doPasswordReset = email => this.auth.sendPasswordResetEmail(email);
+
+ doPasswordUpdate = password => 
+ this.auth.currentUser.updatePassword(password);
 }
 
 export default Firebase;
