651f2e09c3ce6a648b3b316a7493b626ea45ea2d
Chantz Baudoux <cbaud001@plattsburgh.edu>
2020-03-03 16:08:40 -0500

    changed keys

--- backend/functions.py
+++ backend/functions.py
@@ -1,6 +1,6 @@
 
 import requests, json
-from config import *
+from .config import *
 
 
 BASE_URL = 'https://paper-api.alpaca.markets'
@@ -35,8 +35,8 @@ def get_orders():
     orders_info = json.loads(r.content)
     return orders_info
 
-# response = create_order("AMD", 50, "buy", "market", "gtc")
+# response = create_order("AMD", 10, "buy", "market", "gtc")
 response2 = get_orders()
 
 # print(response)
-# print(response2)
+print(response2)
