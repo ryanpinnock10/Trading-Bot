928ed898644a514fc04e8dd35ffbb18c985f8bdc
Chantz Baudoux <cbaud001@plattsburgh.edu>
2020-03-03 16:13:35 -0500

    live connection

--- backend/firebase_conn.py
+++ backend/firebase_conn.py
@@ -1,12 +1,13 @@
 import json
 from firebase import firebase
-from functions import *
+from .functions import *
 
 FIREBASE_URL = "https://trade-bot-a7ed8.firebaseio.com/"
 firebase = firebase.FirebaseApplication(FIREBASE_URL, None)
 
 def input_alpaca_data():    
     #don't run unless you're using a different Alpaca account. Will duplicate entry
+
     data = firebase.post('Alpaca_user', account_info)
 
 def input_alpaca_orders():
@@ -14,5 +15,3 @@ def input_alpaca_orders():
 
 
 input_alpaca_orders()
-
-
