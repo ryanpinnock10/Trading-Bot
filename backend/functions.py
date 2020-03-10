4dba9a58274cb06d885179bfd270b2273a72c368
Chantz Baudoux <cbaud001@plattsburgh.edu>
2020-03-10 15:36:28 -0400

    watchlist functions

--- backend/functions.py
+++ backend/functions.py
@@ -8,6 +8,7 @@ ACCOUNT_URL = '{}/v2/account'.format(BASE_URL)
 ORDERS_URL = '{}/v2/orders'.format(BASE_URL)
 POSITIONS_URL = '{}/v2/positions'.format(BASE_URL)
 WATCHLIST_URL = '{}/v2/watchlists'.format(BASE_URL)
+ADD_ASSET_URL = '{}/v2/watchlists:by_name'.format(BASE_URL)
 # ALL_ORDERS_URL = '{}/v2/orders:by_client_order_id'.format(BASE_URL)
 HEADERS = {"APCA-API-KEY-ID" : API_KEY, "APCA-API-SECRET-KEY" : SECRET_KEY}
 
@@ -36,23 +37,39 @@ def get_orders():
     orders_info = json.loads(r.content)
     return orders_info
 
-def make_watchlist(name, symbols):
-    data = {
-        "name":name, 
-        "symbol":symbols
-    }
+def view_watchlists():
     r = requests.get(WATCHLIST_URL, headers=HEADERS)
     watch_list_info = json.loads(r.content)
     return watch_list_info
 
-response = make_watchlist("Test watchlist", 'AMZN')
+def add_watchlist(name, symbol):
+    data = {
+        "name":name, 
+        "symbol":symbol
+    }
+    r = requests.post(WATCHLIST_URL, header=HEADERS)
+    add_watchlist_info = json.loads(r.content)
+    return add_watchlist_info
+
+def watchlist_add(name, symbol):
+    data = {
+        "name":name, 
+        "symbol":symbol
+    }
+    r = requests.post(ADD_ASSET_URL, headers=HEADERS)
+    asset_info = json.loads(r.content)
+    return asset_info
+
+# response = watchlist_add("Test watchlist", 'IBM')
+response = view_watchlists()
 print(response)
-
-
 # -------------------------------tests-----------------------------------------
 # response1 = get_account()
 # response2 = create_order("IBM", 10, "buy", "market", "gtc")
 # response3 = get_orders()
+
+# response = make_watchlist("Test watchlist", 'AMZN')
+# print(response)
 
 # print(response1)
 # print(response2)
