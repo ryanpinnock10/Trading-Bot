c7ed33943a7120b732c858b34259bab8b84d99a2
jpaiva14 <jpaiv001@plattsburgh.edu>
2020-03-31 16:28:12 -0400

    index2

--- frontend/firebase-react-user/src/components/Landing/index.js
+++ frontend/firebase-react-user/src/components/Landing/index.js
@@ -1,11 +1,73 @@
 import React from 'react';
 //import Logo from  './../AppPictures';
 
-const Landing = () => (
-  <div>
-    <h1>Landing</h1>
+ const Landing = () => (
+   <div>
+     <h1>Landing</h1>
  
-  </div>
-);
+   </div>
+ );
 
 export default Landing;
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { Link, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../store/actions/auth';
+
+const { Header, Content, Footer } = Layout;
+
+class CustomLayout extends React.Component {
+    render() {
+        return (
+            <Layout className="layout">
+                <Header>
+                <div className="logo" />
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    style={{ lineHeight: '64px' }}
+                >
+    
+                {
+                    this.props.isAuthenticated ?
+    
+                    <Menu.Item key="2" onClick={this.props.logout}>
+                        Logout
+                    </Menu.Item>
+    
+                    :
+    
+                    <Menu.Item key="2">
+                        <Link to="/login">Login</Link>
+                    </Menu.Item>
+                }
+    
+                    <Menu.Item key="1">
+                        <Link to="/">Posts</Link>
+                    </Menu.Item>
+                    
+                </Menu>
+                </Header>
+                <Content style={{ padding: '0 50px' }}>
+                <Breadcrumb style={{ margin: '16px 0' }}>
+                    <Breadcrumb.Item><Link to="/">Home</Link></Breadcrumb.Item>
+                    <Breadcrumb.Item><Link to="/">List</Link></Breadcrumb.Item>
+                </Breadcrumb>
+                    <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>
+                        {this.props.children}
+                    </div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                Ant Design ©2016 Created by Ant UED
+                </Footer>
+            </Layout>
+        );
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        logout: () => dispatch(actions.logout()) 
+    }
+}
